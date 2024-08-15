c2801d35a380110efe53feabd9953f16cdf03ce8
TIAGO AGUSTIN GHELMAN <48245701@ort>
2024-08-15 15:00:23 -0300

    instalacion

---
+++ Models/Bd.cs
@@ -0,0 +1,8 @@
+using System.Data.SqlClient;
+using Dapper;
+
+static public class Bd
+{
+private static string _connectionString = @"Server=localhost; DataBase=JJOO; Trusted_Connection=True;";
+}
+
