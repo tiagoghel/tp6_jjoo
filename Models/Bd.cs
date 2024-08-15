f73c1043da5ee0b4adaac7ac0797f94f7e953c94
BRUNO DUILIO MASSACCESI <48590334@ort>
2024-08-15 15:49:15 -0300

    instalacion

--- Models/Bd.cs
+++ Models/Bd.cs
@@ -1,10 +1,20 @@
 using System.Data.SqlClient;
 using Dapper;
 
+namespace tp6_jjoo;
 static public class Bd
 {
 private static string _connectionString = @"Server=localhost; DataBase=JJOO; Trusted_Connection=True;";
+
+
+public static void AgregarDeportista(deportista dep)
+{   
+  
+    using(SqlConnection Bd = new SqlConnection(_connectionString))
+    {
+        string sql = "INSERT INTO DEPORTISTAS (NOMBRE, APELLIDO) VALUES (TIAGO, GHELMAN)";
+        Bd.Execute(sql);
+    }
 }
 
-
-
+}
--- obj/Debug/net8.0/tp6_jjoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/tp6_jjoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc74711a3ef47eb41b8cee652d391ea2475bd83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ade168db1ef06756340766a83e46a3eaff8a543")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
