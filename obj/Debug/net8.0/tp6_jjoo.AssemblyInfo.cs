9beb5cac62abe33b41986de0e0085fb30252eab9
BRUNO DUILIO MASSACCESI <48590334@ort>
2024-08-27 07:44:54 -0300

    conexion

--- obj/Debug/net8.0/tp6_jjoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/tp6_jjoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2b2681fd0459e91bf700f3ac9889564d2f73f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4c9dae4a5f1850bf463375229c21527d9178fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp6_jjoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
